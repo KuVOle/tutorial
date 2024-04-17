d1640d4e0e5326741344ea2ec29592a523fa22e3
KuVOle <traviscolt95@yandex.ru>
2024-04-17 21:58:43 +0300

    add auth controller stract

---
+++ server/src/controller/auth_controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+
+class AuthController {
+	login(req: Request, res: Response, next: NextFunction) {
+		res.send("login");
+	}
+	restoreGet(req: Request, res: Response, next: NextFunction) {
+		res.send("restoreGet");
+	}
+	restorePost(req: Request, res: Response, next: NextFunction) {
+		res.send("restorePost");
+	}
+	forgot(req: Request, res: Response, next: NextFunction) {
+		res.send("forgot");
+	}
+	registration(req: Request, res: Response, next: NextFunction) {
+		res.send("registration");
+	}
+}
+
+export default new AuthController();
--- server/src/router/auth_router.ts
+++ server/src/router/auth_router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import auth_controller from "../controller/auth_controller.js";
 
 const router = Router();
 
-router.post("/login");
-router.post("/registration");
-router.post("/restore");
+router.post("/login", auth_controller.login);
+router.post("/registration", auth_controller.registration);
+router.post("/forgot", auth_controller.forgot);
+router.get("/restore", auth_controller.restoreGet);
+router.post("/restore", auth_controller.restorePost);
 
 export default router;
