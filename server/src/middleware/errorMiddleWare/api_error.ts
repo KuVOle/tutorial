f33da40ef70278df31b6f20a504e285d74b35c56
KuVOle <traviscolt95@yandex.ru>
2024-04-18 10:53:45 +0300

    add client

--- server/src/middleware/errorMiddleWare/api_error.ts
+++ server/src/middleware/errorMiddleWare/api_error.ts
@@ -1,15 +1,18 @@
 class ApiError {
-  status: number;
-  message: string;
-  constructor(status: number, message: string) {
-    this.status = status;
-    this.message = message;
-  }
-  static serverError(message: string): ApiError {
-    return new ApiError(404, message);
-  }
-  static transportError(message: string): ApiError {
-    return new ApiError(500, message);
-  }
+	status: number;
+	message: string;
+	constructor(status: number, message: string) {
+		this.status = status;
+		this.message = message;
+	}
+	static serverError(message: string): ApiError {
+		return new ApiError(400, message);
+	}
+	static notFoundError(message: string): ApiError {
+		return new ApiError(404, message);
+	}
+	static transportError(message: string): ApiError {
+		return new ApiError(500, message);
+	}
 }
 export { ApiError };
