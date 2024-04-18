3afb8a0963a5d03320a580962f3dc8e0dde7d261
KuVOle <traviscolt95@yandex.ru>
2024-04-18 11:35:12 +0300

    rebuild client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import logger from "./utils/logger.js";
+import errorMiddleWare from "./middleware/errorMiddleWare/error_middleware.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
 	res.send("ok");
 });
+
+app.use(errorMiddleWare);
 
 app.listen(3000, () => {
 	logger.info("abc");
